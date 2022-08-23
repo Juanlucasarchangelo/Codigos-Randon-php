ea8f8fdfdd42dc1cba27cf923df66d2b346fd766
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-08-22 21:42:06 -0300

    Uso de constantes e ifs

--- Aula 13/index.php
+++ Aula 13/index.php
@@ -1,5 +1,22 @@
 <?php
 
+//Definindo constantes para a aplicação
+define("min", 17);
+define("max", 45);
 
+$idade = 25;
+
+echo 'MIM: ' . min . '<br>';
+echo 'MAX: ' . max . '<br>';
+echo 'IDADE: ' . $idade . '<br>';
+
+if ($idade > min) {
+    echo 'Acesso liberado...';
+} else {
+    echo 'Acesso negado...';
+}
+
+//Mostra a versão do  php (7.1)
+echo '<br>' . PHP_VERSION;
 
 ?>
