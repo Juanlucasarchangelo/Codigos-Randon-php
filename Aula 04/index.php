fecf00319f10bd57cff524548996d5c633cd5529
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-08-22 21:34:03 -0300

    Trabalhando com php e html juntos

---
+++ Aula 04/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$mult = 5 * 2;
+$divisao = 5 / 2;
+$sub = 5 - 2;
+$adicao = 5 + 2;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo '5 * 2 = ' . $mult . '<br>';   ?>
+    <?php echo '5 / 2 = ' . $divisao . '<br>';   ?>
+    <?php echo '5 - 2 = ' . $sub . '<br>';   ?>
+    <?php echo '5 + 2 = ' . $adicao . '<br>';   ?>
+</body>
+</html>
