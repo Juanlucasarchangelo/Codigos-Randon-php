a36144c8f83a71566a0d07d89ae4af3482112cce
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-08-22 22:05:14 -0300

    uso do Switch no code

---
+++ Aula 15/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$dia = 1;
+
+switch ($dia){
+    case 1:
+        echo "1";
+        break;
+    case 2:
+        echo "2";
+        break;
+    default:
+        echo "Nada encontrado";
+
+}
+
+?>
