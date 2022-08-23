8f68ff40e7adb4ae35fb38572427d7811b6008cf
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-08-22 21:22:21 -0300

    Comando gettype

---
+++ Aula 03/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$nomeCompleto = 'juan';
+$sexo = 'm';
+$salario = 3500.00;
+$valido = false;
+
+//Exibe o tipo da váriavel (gettype)
+echo gettype($valido);
+
+echo $nomeCompleto, $salario, $sexo;
+
+?>
