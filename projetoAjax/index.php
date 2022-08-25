e3b227686f349aaf44856471e646de0659040855
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-08-25 20:36:09 -0300

    refresh

---
+++ projetoAjax/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Metas -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Title -->
+    <title>AJAX com PHP</title>
+
+    <!-- Scrypts -->
+    <script src="js/jquery.js"></script>
+    <script src="js/script.js"></script>
+
+</head>
+<body>
+    <h1>Ajax com PHP - Juan Archangelo</h1>
+
+    <form action="">
+        <label for="nome">Nome completo</label>
+        <input type="text" id="nome" placeholder="Nome">
+
+        <label for="email">E-mails</label>
+        <input type="email" id="email" placeholder="E-mail principal">
+
+        <label for="telefone">Telefone</label>
+        <input type="phone" id="telefone" placeholder="Telefone (Whats)">
+
+        <input type="submit" name="submit" value="ENVIAR">
+    </form>
+</body>
+</html>
