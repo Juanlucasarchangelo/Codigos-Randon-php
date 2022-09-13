558406c89786a475dbc21dedb38f77a73dc2bcb3
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-09-13 13:32:55 -0300

    Codigo descenessário

---
+++ Projeto PHP/teste.php
@@ -0,0 +1,16 @@
+<?php
+
+class operacoesMatematicas{
+  
+  public $soma;
+  
+  function multiply($a, $b) {
+  echo $this->soma = $a * $b;
+  }
+}
+  
+  $objOperacoes = new operacoesMatematicas;
+
+  $objOperacoes->multiply(2, 5);
+  
+?>
